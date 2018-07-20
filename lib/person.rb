b8ad5b50bfb6203fd40aae707313b9229a414ef5
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-20 02:33:31 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(human_name)
+    @name = human_name
+  end
+  def name
+    @name
+  end
+
+  def job= (human_job)
+    @job = human_job
+  end
+
+  def job
+    @job
+  end
+
+end
