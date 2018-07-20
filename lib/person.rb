04fb1ab2bf449b37a473eed84767ecbe636a93b2
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-20 02:31:57 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(human_name)
+    @name = human_name
+  end
+  def name
+    @name
+  end
+
+  def job=(human_job)
+    @job = human_job
+  end
+
+  def breed
+    @job
+  end
+
+end
